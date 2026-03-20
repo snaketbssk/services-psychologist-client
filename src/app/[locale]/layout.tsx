c92ce103fdf40dcbf37ef402aa2573c8d3789bf4
Vladimir Tereshin <snaketbssk@gmail.com>
2026-03-20 07:54:30 +0400

    new

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -51,7 +51,7 @@ export default async function LocaleLayout({
 }) {
   const { locale } = await params;
 
-  if (!routing.locales.includes(locale as "en" | "ru")) {
+  if (!routing.locales.includes(locale as "en" | "ru" | "am")) {
     notFound();
   }
 
