d379846d5ed303b796726363c2210f76d63ab079
Vladimir Tereshin <snaketbssk@gmail.com>
2026-03-20 08:33:35 +0400

    new

--- src/components/BookConsultationDialog.tsx
+++ src/components/BookConsultationDialog.tsx
@@ -20,7 +20,10 @@ export default function BookConsultationDialog({ trigger }: Props) {
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent
           showCloseButton
-          className="w-[calc(100vw-2rem)] max-w-5xl sm:max-w-5xl p-0 overflow-hidden rounded-[20px]"
+          className="w-[calc(100vw-2rem)] max-w-5xl sm:max-w-5xl p-0 overflow-hidden rounded-[20px]
+            duration-300
+            data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-open:slide-in-from-bottom-6
+            data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 data-closed:slide-out-to-bottom-4"
         >
           <BookConsultation />
         </DialogContent>
